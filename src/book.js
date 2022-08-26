a3b0e09625542aaacd54dadb52140b5a2bdd56bd
Brett Kuhn <brettkuhn87@gmail.com>
2022-08-25 22:03:07 -0600

    Complete book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,53 @@
-function createTitle() {
-  
+function createTitle(title) {
+  var modifiedTitle = `The ${title}`
+  return modifiedTitle
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return mainCharacter
+}
+
+// each time function is called it retains the updatedd array elements!!
+// learned that !operator before method NEGATES it - tried for statement and it didn't work for some reason
+function saveReview(review1, arrayForReviews) {
+  if (!arrayForReviews.includes(review1)) {
+      arrayForReviews.push(review1);
+      console.log(arrayForReviews);
+      return arrayForReviews
+}
+}
+
+//is this too easy?
+function calculatePageCount(title) {
+  var pageCount = title.length*20
+  return pageCount
+}
+
+function writeBook(bookTitle, mainCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  }
+  return book
+}
+
+function editBook(bookToEdit) {
+  bookToEdit.pageCount = bookToEdit.pageCount * .75
+  return bookToEdit
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
