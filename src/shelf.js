773442bfcc034208c7e929434779f0188877b874
Brett Kuhn <brettkuhn87@gmail.com>
2022-08-26 00:01:08 -0600

    Completed shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,44 @@
 
+function shelfBook(book, shelfArray) {
+  if (shelfArray.length < 3) {
+    shelfArray.unshift(book);
+    return shelfArray
+  }
+}
+
+function unshelfBook(removedBookName, shelfArray) {
+  for (var i = 0; i < shelfArray.length; i++) {
+    if (shelfArray[i].title === removedBookName){
+      shelfArray.splice(i, 1)
+      }
+    }
+  return shelfArray
+  }
+
+function listTitles(shelfArray) {
+  var arrayListOfTitles = [];
+    for (var i = 0; i < shelfArray.length; i++){
+      arrayListOfTitles.push(shelfArray[i].title)
+    }
+  var textListOfTitles = arrayListOfTitles.join(", ")
+  return textListOfTitles
+}
+
+function searchShelf(shelfArray, bookTitle) {
+arrayListOfTitles = [];
+  for (var i = 0; i < shelfArray.length; i++){
+    arrayListOfTitles.push(shelfArray[i].title)
+  }
+if (arrayListOfTitles.includes(bookTitle)){
+  return true
+  } else {
+  return false
+  }
+}
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
